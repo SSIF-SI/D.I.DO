443f37b5a6f23e31c2ddaa5041d9d483c59a6a43
Fedez1879 <volpini.federico79@gmail.com>
2016-09-23 16:47:18 +0200

    Fixed signatureTest After last mod

--- dido/src/test/java/dido/signatureTests/SignatureTest1.java
+++ dido/src/test/java/dido/signatureTests/SignatureTest1.java
@@ -6,7 +6,8 @@ import dido.signature.SignatureManager;
 public class SignatureTest1 {
 	public static final String SRC = "/testresources/sample06.pdf";
 	public static void main(String[] args) {
-		SignatureManager sigMan=new SignatureManager(SRC);
+		SignatureManager sigMan=new SignatureManager();
+		sigMan.loadPDF(SRC);
 	}
 
 }
