b679c5117637d99e58f1a00f42c4218e41096192
liparig <lipari.giuseppe@gmail.com>
2016-09-23 14:40:40 +0200

    added log4j in signaturemanager

--- dido/src/main/java/dido/signature/SignatureManager.java
+++ dido/src/main/java/dido/signature/SignatureManager.java
@@ -9,6 +9,7 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.bouncycastle.asn1.pkcs.RSAPublicKey;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.tsp.TimeStampToken;
@@ -26,132 +27,128 @@ import com.itextpdf.text.pdf.security.SignaturePermissions;
 import com.itextpdf.text.pdf.security.SignaturePermissions.FieldLock;
 
 import dido.signature.interfaces.iSignatureManager;
-//TODO: DA COMPLETARE!! Ho solo copiato i metodi dagli esempi di utilizzo della libreria iText,forse sono da integrare con altre funzionalità.
 
 public class SignatureManager implements iSignatureManager {
+	final static Logger logger = Logger.getLogger(SignatureManager.class);
 	private String pdfPath = null;
 	private List<Signature> signatures = null;
 	private Signature tmpSignature = null;
-	
-	public PdfPKCS7 verifySignature(AcroFields fields, String name) throws GeneralSecurityException, IOException {
-		System.out.println("Signature covers whole document: " + fields.signatureCoversWholeDocument(name));
+	public SignatureManager(String pdfPath){
+		super();
+		loadPDF(pdfPath);
+
+	}
+
+	private PdfPKCS7 verifySignature(AcroFields fields, String name) throws GeneralSecurityException, IOException {
+		logger.info("Signature covers whole document: " + fields.signatureCoversWholeDocument(name));
 		this.tmpSignature.setName(name);
-		
-		System.out.println("Document revision: " + fields.getRevision(name) + " of " + fields.getTotalRevisions());
+
+		logger.info("Document revision: " + fields.getRevision(name) + " of " + fields.getTotalRevisions());
 		this.tmpSignature.setRevision(fields.getRevision(name));
 		this.tmpSignature.setTotalREvisions(fields.getTotalRevisions());
-        
+
 		PdfPKCS7 pkcs7 = fields.verifySignature(name);
-        System.out.println("Integrity check OK? " + pkcs7.verify());
-        this.tmpSignature.setComplete(pkcs7.verify());
-        
-        return pkcs7;
+		logger.info("Integrity check OK? " + pkcs7.verify());
+		this.tmpSignature.setComplete(pkcs7.verify());
+
+		return pkcs7;
 	}
- 
-	public void verifySignatures(String path) throws IOException, GeneralSecurityException {
-		System.out.println(path);
-	        PdfReader reader = new PdfReader(path);
-	        AcroFields fields = reader.getAcroFields();
-	        ArrayList<String> names = fields.getSignatureNames();
-			for (String name : names) {
-				this.tmpSignature = new Signature();
-				System.out.println("===== " + name + " =====");
-				verifySignature(fields, name);
-				this.signatures.add(this.tmpSignature);
-			}
-		System.out.println();
-	}
-	
-	public SignaturePermissions inspectSignature(AcroFields fields, String name, SignaturePermissions perms) throws GeneralSecurityException, IOException {
+
+	private SignaturePermissions inspectSignature(AcroFields fields, String name, SignaturePermissions perms) throws GeneralSecurityException, IOException {
 		List<FieldPosition> fps = fields.getFieldPositions(name);
 		if (fps != null && fps.size() > 0) {
 			FieldPosition fp = fps.get(0);
 			Rectangle pos = fp.position;
 			if (pos.getWidth() == 0 || pos.getHeight() == 0) {
-				System.out.println("Invisible signature");
+				logger.info("Invisible signature");
 				this.tmpSignature.setInvisible(true);
 			}
 			else {
 				this.tmpSignature.setInvisible(false);
-				System.out.println(String.format("Field on page %s; llx: %s, lly: %s, urx: %s; ury: %s",
-					fp.page, pos.getLeft(), pos.getBottom(), pos.getRight(), pos.getTop()));
+				logger.info(String.format("Field on page %s; llx: %s, lly: %s, urx: %s; ury: %s",
+						fp.page, pos.getLeft(), pos.getBottom(), pos.getRight(), pos.getTop()));
 			}
 		}
- 
+
 		PdfPKCS7 pkcs7 = this.verifySignature(fields, name);
-		
-		System.out.println("Digest algorithm: " + pkcs7.getHashAlgorithm());
+
+		logger.info("Digest algorithm: " + pkcs7.getHashAlgorithm());
 		this.tmpSignature.setHashAlgorithm(pkcs7.getHashAlgorithm());
-		
-		System.out.println("Encryption algorithm: " + pkcs7.getEncryptionAlgorithm());
+
+		logger.info("Encryption algorithm: " + pkcs7.getEncryptionAlgorithm());
 		this.tmpSignature.setEncryptionAlgorithm(pkcs7.getEncryptionAlgorithm());
-		
-		System.out.println("Filter subtype: " + pkcs7.getFilterSubtype());
+
+		logger.info("Filter subtype: " + pkcs7.getFilterSubtype());
+		if(pkcs7.getFilterSubtype()!=null)
 		this.tmpSignature.setFilterSubtype(pkcs7.getFilterSubtype().toString());
-		
+
 		X509Certificate cert = (X509Certificate) pkcs7.getSigningCertificate();
-		System.out.println("Name of the signer: " + CertificateInfo.getSubjectFields(cert).getField("CN"));
+		logger.info("Name of the signer: " + CertificateInfo.getSubjectFields(cert).getField("CN"));
 		this.tmpSignature.setSigner(CertificateInfo.getSubjectFields(cert).getField("CN"));
-		
+
 		RSAPublicKey pub = (RSAPublicKey) cert.getPublicKey();
-		System.out.println("Public Key: " + pub.getModulus().toString(16));
+		logger.info("Public Key: " + pub.getModulus().toString(16));
 		this.tmpSignature.setPublicKey(pub.getModulus().toString(16));
-		
+
 		if (pkcs7.getSignName() != null){
-			System.out.println("Alternative name of the signer: " + pkcs7.getSignName());
+			logger.info("Alternative name of the signer: " + pkcs7.getSignName());
 			this.tmpSignature.setSignerAlt(pkcs7.getSignName());
 		} else {
 			this.tmpSignature.setSignerAlt(null);
 		}
-		
+
 		SimpleDateFormat date_format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SS");
-		System.out.println("Signed on: " + date_format.format(pkcs7.getSignDate().getTime()));
+		logger.info("Signed on: " + date_format.format(pkcs7.getSignDate().getTime()));
 		this.tmpSignature.setSignDate(date_format.format(pkcs7.getSignDate().getTime()));
-		
+
 		if (pkcs7.getTimeStampDate() != null) {
-			System.out.println("TimeStamp: " + date_format.format(pkcs7.getTimeStampDate().getTime()));
+			logger.info("TimeStamp: " + date_format.format(pkcs7.getTimeStampDate().getTime()));
 			TimeStampToken ts = pkcs7.getTimeStampToken();
-			System.out.println("TimeStamp service: " + ts.getTimeStampInfo().getTsa());
-			System.out.println("Timestamp verified? " + pkcs7.verifyTimestampImprint());
+			logger.info("TimeStamp service: " + ts.getTimeStampInfo().getTsa());
+			logger.info("Timestamp verified? " + pkcs7.verifyTimestampImprint());
 		}
-		System.out.println("Location: " + pkcs7.getLocation());
-		System.out.println("Reason: " + pkcs7.getReason());
+		logger.info("Location: " + pkcs7.getLocation());
+		logger.info("Reason: " + pkcs7.getReason());
 		PdfDictionary sigDict = fields.getSignatureDictionary(name);
 		PdfString contact = sigDict.getAsString(PdfName.CONTACTINFO);
 		if (contact != null)
-			System.out.println("Contact info: " + contact);
+			logger.info("Contact info: " + contact);
 		perms = new SignaturePermissions(sigDict, perms);
-		
-		System.out.println("Signature type: " + (perms.isCertification() ? "certification" : "approval"));
+
+		logger.info("Signature type: " + (perms.isCertification() ? "certification" : "approval"));
 		this.tmpSignature.setSignType(perms.isCertification() ? "certification" : "approval");
-		
-		System.out.println("Filling out fields allowed: " + perms.isFillInAllowed());
-		System.out.println("Adding annotations allowed: " + perms.isAnnotationsAllowed());
+
+		logger.info("Filling out fields allowed: " + perms.isFillInAllowed());
+		logger.info("Adding annotations allowed: " + perms.isAnnotationsAllowed());
 		for (FieldLock lock : perms.getFieldLocks()) {
-			System.out.println("Lock: " + lock.toString());
+			logger.info("Lock: " + lock.toString());
 		}
-        return perms;
+		return perms;
 	}
- 
-	public void inspectSignatures(String path) throws IOException, GeneralSecurityException {
-		System.out.println(path);
-        PdfReader reader = new PdfReader(path);
-        AcroFields fields = reader.getAcroFields();
-        ArrayList<String> names = fields.getSignatureNames();
-        SignaturePermissions perms = null;
+
+	private void inspectSignatures(String path) throws IOException, GeneralSecurityException {
+		logger.info("Path document to inspect"+path);
+		PdfReader reader = new PdfReader(path);
+		AcroFields fields = reader.getAcroFields();
+		ArrayList<String> names = fields.getSignatureNames();
+		SignaturePermissions perms = null;
 		for (String name : names) {
-			System.out.println("===== " + name + " =====");
+			this.tmpSignature=new Signature();
+			logger.info("===== " + name + " =====");
 			perms = inspectSignature(fields, name, perms);
+			signatures.add(tmpSignature);
 		}
-		System.out.println();
+		logger.info("end ispection");
+
 	}
- 
+
 	public boolean loadPDF(String path){
-		SignatureManager app = new SignatureManager();
+		signatures=new ArrayList<>();
+		pdfPath=path;
 		BouncyCastleProvider provider = new BouncyCastleProvider();
 		Security.addProvider(provider);
 		try {
-			app.inspectSignatures(this.pdfPath);
+			this.inspectSignatures(this.pdfPath);
 		} catch (IOException e) {
 			e.printStackTrace();
 			return false;
@@ -161,7 +158,7 @@ public class SignatureManager implements iSignatureManager {
 		}
 		return true;
 	}
-	
+
 	public List<Signature> getSignatures() {
 		return this.signatures;
 	}
