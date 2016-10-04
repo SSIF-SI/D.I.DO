e30b77b57d3e0e324d9392d2c52f2dff435998be
Fedez1879 <volpini.federico79@gmail.com>
2016-10-04 08:21:21 +0200

    piccoli bugfix

--- dido-php/class/DB/Signature.class.php
+++ dido-php/class/DB/Signature.class.php
@@ -8,32 +8,38 @@ class Signature extends AnyDocument {
 	 * $masterDocumentData = new MasterdocumentData(Connector::getInstance()); $this->_md_data = $masterDocumentData->searchByKeyValue(array( 'id_md'	=> $id, $sigObj->getDescrizioni() ));
 	 */
 	public function getSigners($id_md) {
-		Utils::printr ( $id_md );
 		$sigle = array ();
 		$result = array ();
+		
 		$fixedSigners = new FixedSigners ( Connector::getInstance () );
 		$signatures = $fixedSigners->getAll ();
+		
 		foreach ( $signatures as $k => $v ) {
 			$result [$v ['sigla']] = array (
 					'pkey' => $v ['pkey'],
-					'descrizione' => $v ['descrizione'] 
+					'descrizione' => $v ['descrizione'] ,
+					'email'=> $v ['email']
 			);
 		}
 		$variableSignersRoles = new VariableSignersRoles ( Connector::getInstance () );
-		
 		$sigle = $variableSignersRoles->getRoleDescription ();
-		Utils::printr ( $sigle );
 		
 		$masterDocumentData = new MasterdocumentData ( Connector::getInstance () );
-		$signatures = $masterDocumentData->searchByKeys ( $sigle, $id_md );
+		$signatures = $masterDocumentData->searchByKeys ( array_keys($sigle), $id_md );
+		$signatures = Utils::getListfromField($signatures, 'value');
+		
+		$publickeys=join(',', array_map("Utils::apici",$signatures));
+
+		$signatures = $this->getBy('pkey',$publickeys,'sigla' ) ;
 		
 		foreach ( $signatures as $k => $v ) {
-			$result [$v ['key']] = array (
-					'pkey' => $v ['value'],
-					'descrizione' => array_search ( $v ['key'], $sigle ) 
+			$result [$v ['sigla']] = array (
+					'pkey' => $v ['pkey'],
+					'descrizione' => isset($sigle[$v ['sigla']]) ? $sigle[$v ['sigla']] : null ,
+					'email'=> $v ['email']
 			);
 		}
-		Utils::printr ( $result );
+		
 		return $result;
 	}
 }
