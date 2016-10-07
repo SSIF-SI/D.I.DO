71c1e12ff2f73d421501e2f892f8d6a18cf1e1e2
Fedez1879 <volpini.federico79@gmail.com>
2016-10-07 11:32:41 +0200

    modificata signature.class

--- dido-php/index.php
+++ dido-php/index.php
@@ -2,20 +2,21 @@
 
 require_once ("config.php");
 
-
+/*
 $PDFParser = new PDFParser("/var/lib/tomcat7/webapps/dido-php-test/richiesta_delega_2016_DMT_signed.pdf");
 echo Utils::printr($PDFParser->getSignatures());
 echo Utils::printr($PDFParser->getMetadata());
 var_dump($PDFParser->is_PDFA());
-
+*/
 /*
 $ftp = FTPConnector::getInstance();
 $contents = $ftp->getContents("/CAMPUS");
 Utils::printr(Utils::filterList($contents['contents'],'isPDF',1));
 */
+/*
 Utils::printr(Personale::getInstance()->getPersone());
 Utils::printr(Personale::getInstance()->getGruppi());
-
-FlowChecker::getInstance()->checkMasterDocument(1);
+*/
+FlowChecker::getInstance()->checkMasterDocument(array('id_md' =>1));
 // print_r(Personale::getInstance()->getPersonakey(),1);
 //$md = new Masterdocument(Connector::getInstance());
