63b36bca194a260e85564fc509f01f77706fbc00
Fedez1879 <volpini.federico79@gmail.com>
2016-10-07 09:10:26 +0200

    modificato index.php per testare la nuova classe PDFParser

--- dido-php/index.php
+++ dido-php/index.php
@@ -3,23 +3,10 @@
 require_once ("config.php");
 require_once 'Personale.class.php';
 
-
-$signature = new Java('dido.signature.SignatureManager');
-if($signature->loadPDF("/var/lib/tomcat7/webapps/dido-php-test/richiesta_delega_2016_DMT_signed.pdf")){
-	$list = json_decode($signature->getSignatures());
-	echo"<pre>".print_r($list,1)."</pre>";
-	$content = json_decode($signature->getXmlMetadata());
-	
-	$xmp_data_start = strpos($content, '<x:xmpmeta');
-	$xmp_data_end   = strpos($content, '</x:xmpmeta>');
-	$xmp_length     = $xmp_data_end - $xmp_data_start;
-	$xmp_data       = substr($content, $xmp_data_start, $xmp_length + 12);
-	$xmp_data = str_replace(":","_",$xmp_data);
-	$xmp            = simplexml_load_string($xmp_data);	
-	
-	echo"<pre>PDF di tipo: ".$xmp->rdf_RDF->rdf_Description['pdfaid_conformance']."</pre>";
-
-}
+$PDFParser = new PDFParser("/var/lib/tomcat7/webapps/dido-php-test/richiesta_delega_2016_DMT_signed.pdf");
+echo Utils::printr($PDFParser->getSignatures());
+echo Utils::printr($PDFParser->getMetadata());
+var_dump($PDFParser->is_PDFA());
 
 /*
 $ftp = FTPConnector::getInstance();
