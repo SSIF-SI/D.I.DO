25564d6c07f9f4a7f968706378a8d5b7aa19073f
liparig <lipari.giuseppe@gmail.com>
2017-03-21 17:18:09 +0100

    Little fix

--- dido-php/class/Helpers/PersonaleHelper.class.php
+++ dido-php/class/Helpers/PersonaleHelper.class.php
@@ -1,6 +1,8 @@
 <?php 
 class PersonaleHelper{
 	static function getNominativo($id){
+		if(empty($id))
+			return "--Nessuno--";
 		$result = Personale::getInstance()->getPersona($id)['cognome']." ".Personale::getInstance()->getPersona($id)['nome'];
 		$result = trim($result);
 		return empty($result) ? "<div class='alert alert-danger'>Utente non più attivo. Rimuovere.</div>": $result;
