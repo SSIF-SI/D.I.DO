62a8b7abc6b0d50a8ea7e798efecde4e8aefb57e
Fedez1879 <volpini.federico79@gmail.com>
2017-05-08 11:11:04 +0200

    aggiunto ftpConnector

--- dido-php/class/main/ProcedureManager/abstract/AProcedureManager.class.php
+++ dido-php/class/main/ProcedureManager/abstract/AProcedureManager.class.php
@@ -5,13 +5,19 @@ abstract class AProcedureManager{
 	const INCOMPLETE = -1;
 	
 	private $_dbConnector = null;
+	private $_ftpConnector = null;
 	
-	public function __construct(IDBConnector $dbConnector){
+	public function __construct(IDBConnector $dbConnector, IFTPConnector $ftpConnector){
 		$this->_dbConnector = $dbConnector;
+		$this->_ftpConnector = $ftpConnector;
 	}
 	
-	public function getConnector(){
+	public function getDBConnector(){
 		return $this->_dbConnector;
+	}
+	
+	public function getFTPConnector(){
+		return $this->_ftpConnector;
 	}
 	
 	abstract public function create($main, $data, $uploadPath);
