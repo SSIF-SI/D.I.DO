7154159e8bb73fb84ed70b30951420ac55c1c4f7
Fedez1879 <volpini.federico79@gmail.com>
2017-05-08 10:21:58 +0200

    Classe astratta AProcedureManager

--- dido-php/class/main/ProcedureManager/abstract/AProcedureManager.class.php
+++ dido-php/class/main/ProcedureManager/abstract/AProcedureManager.class.php
@@ -6,8 +6,8 @@ abstract class AProcedureManager{
 		$this->_dbConnector = $dbConnector;
 	}
 	
-	abstract public function create($main, $data, $uploadPath = null);
-	abstract public function update($data, $main = null, $uploadPath = null);
+	abstract public function create($main, $data, $uploadPath);
+	abstract public function update($data, $uploadPath = null);
 	abstract public function delete($main);
 }
 ?>
