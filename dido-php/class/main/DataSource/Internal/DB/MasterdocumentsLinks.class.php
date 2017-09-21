ebe6c5f9aaf4ba7b3b8d161e7d50020e0a950c8f
liparig <lipari.giuseppe@gmail.com>
2017-09-21 12:33:10 +0200

    New MasterDoumentsLinks class

---
+++ dido-php/class/main/DataSource/Internal/DB/MasterdocumentsLinks.class.php
@@ -0,0 +1,19 @@
+<?php
+class MasterDocumentsLinks extends Crud {
+
+	const ID_LINK = "id_link";
+
+	const ID_FATHER = "id_md_father";
+
+	const ID_CHILD = "id_md_child";
+
+	protected $TABLE = "master_documents_links";
+
+	protected $SEQ_NAME = "master_documents_links_id_link_seq";
+	
+	public function __construct($connInstance) {
+		parent::__construct ( $connInstance );
+	}
+}
+
+?>
