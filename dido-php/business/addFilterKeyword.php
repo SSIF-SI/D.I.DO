980ed85a56a04e5a03f98ce04b1802e6f014e7d2
liparig <lipari.giuseppe@gmail.com>
2017-10-27 18:36:47 +0200

    Da fare l'associazione select keyword con una select di valori e modificare lo script per l'aggiunta/rimozione del filtro

--- dido-php/business/addFilterKeyword.php
+++ dido-php/business/addFilterKeyword.php
@@ -1,4 +1,62 @@
 <?php 
 require_once("../config.php");
 if(!Utils::checkAjax()) die();
+
+$className = $_GET['source'];
+$dataclassName=$className."Data";
+
+$A = new Application();
+$D = new $className($A->getDBConnector());
+$D_data=new $dataclassName($A->getDBConnector());
+
+if(isset($_GET['closed'])){
+	$listIdDoc=$D->getBy("closed", $_GET['closed'], 'id_doc');
+	$listIdDoc=	Utils::getListfromField($listIdDoc,"id_doc","id_doc");
+	$ids=implode(",", $listIdDoc);
+	$listdoc_data=$D_data->getBy("id_doc",$ids,'id_doc');
+}
+else {
+	$listIdDoc=$D->getAll();
+	$listIdDoc=	Utils::getListfromField($listIdDoc,"id_doc","id_doc");
+	$ids=implode(",", $listIdDoc);
+}
+
+$keys = $D_data->getRealDistinct("key","id_doc IN (".$ids." )");
+$keys=Utils::getListfromField($keys,"key");
 ?>
+
+<form>
+	<div class="form-group">
+		<label>Parole Chiave</label>
+		<div class="select">
+		<select id="select" name="kw-option" >
+<?php 
+	foreach($keys as $k=>$val):
+	$option=ucwords($val);
+?>
+	<option id="kw-<?=$k?>"  value="<?=$val?>"><?=$option?></option>
+
+<?php endforeach;?>
+<!-- TODO: MODIFICARE SCRIPT  E SELECT PER ASSOCIAZIONE VALORI -->
+</select>
+</div>
+
+	</div>
+	<div id="filterResult" class="btn-warning">
+	</div>
+</form>
+<script>
+	$(".filter-box input").each(function(el){
+		var idToRemove =  $(this).attr("id").replace(/filter-kw-/,"kw-");
+		$("#"+idToRemove).remove();
+	});
+	$(".select input").click(function(e){
+		var action = $(this).prop("kw-option");
+		echo action;
+		if(action)
+			$('<input id="filter-'+$(this).attr("id")+'" type="hidden" name="nome['+$(this).attr("id")+']" value="'+$(this).val()+'" />').appendTo($("#filterResult"));
+		else 
+			$("#filter-"+$(this).attr("id")).remove();
+		
+	});
+</script>
